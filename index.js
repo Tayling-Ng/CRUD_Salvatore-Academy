ccf2e0dcd2994c1fe88a5c45af8b74bd05413bc0
Tayling Ng <taymng@yahoo.com.br>
2024-05-08 21:06:19 +0000

    Endpoint Read All '[GET] /personagem'

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
+// Requisição: http://localhost:3000
 app.get('/', function (req, res) {
-    res.send('Olá')
+    res.send('Hi Folks!')
+})
+
+const lista = ['Java', 'Kotlin', 'Android']
+
+// Endpoint Read All: [GET] /personagem
+// Requisição: http://localhost:3000/personagem
+app.get('/personagem', function (req, res) {
+    res.send(lista)
 })
 
 app.listen(3000)
