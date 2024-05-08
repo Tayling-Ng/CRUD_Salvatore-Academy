b1051e753ba39f5f6e15db0cdadda78c358c9702
Tayling Ng <taymng@yahoo.com.br>
2024-05-08 22:15:30 +0000

    Endpoint Create '[POST] /personagem'

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 
-// Requisição: http://localhost:3000
+// Receber o corpo da requisição em json
+app.use(express.json())
+
+// Requisição GET: http://localhost:3000
 app.get('/', function (req, res) {
     res.send('Hi Folks!')
 })
@@ -9,13 +12,13 @@ app.get('/', function (req, res) {
 const lista = ['Java', 'Kotlin', 'Android']
 
 // Endpoint Read All: [GET] /personagem
-// Requisição: http://localhost:3000/personagem
+// Requisição GET: http://localhost:3000/personagem
 app.get('/personagem', function (req, res) {
     res.send(lista)
 })
 
 // Endpoint Read by ID: [GET] /personagem/:id
-// Requisição: http://localhost:3000/personagem/id
+// Requisição GET: http://localhost:3000/personagem/id
 app.get('/personagem/:id', function (req, res) {
     
     const id = req.params.id // Acessando o parâmetro id
@@ -23,6 +26,22 @@ app.get('/personagem/:id', function (req, res) {
     const item = lista[id - 1] // Acessando o item da lista usando id - 1
 
     res.send(item)
+})
+
+// Endpoint Create: [POST] /personagem
+// Escrever o corpo da requisição
+// Requisição POST: http://localhost:3000/personagem
+app.post('/personagem', function (req, res) {
+
+    const body = req.body // Acessando o corpo da requisição
+    // console.log(body)
+
+    const novoItem = body.nome // Acessando a propriedade nome no corpo da requisião
+
+    lista.push(novoItem) // Adiciona o valor da propriedade na lista
+
+    res.send('Item adicionado com sucesso!: ' + novoItem)
+    // console.log(lista)
 })
 
 app.listen(3000)
